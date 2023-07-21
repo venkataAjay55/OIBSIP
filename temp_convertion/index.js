32848c9c06907bf789abd25adbcdfff4d30f5885
venkataAjay55 <91135333+venkataAjay55@users.noreply.github.com>
2023-07-21 18:26:30 +0530

    Create index.js

---
+++ temp_convertion/index.js
@@ -0,0 +1,35 @@
+
+document.getElementById('convert').onclick = tempConvert;
+document.getElementById('reset').onclick = clearForm;
+
+function tempConvert() {
+    
+    var fahrenheit = document.getElementById("fahrenheit").value;
+    var celsius = document.getElementById("celsius").value;
+    var kelvin = document.getElementById("kelvin").value;
+  
+  if (fahrenheit != '') {
+        celsius = (parseFloat(fahrenheit) - 32) / 1.8;
+        kelvin = ((parseFloat(fahrenheit)-32))*(5/9)+273.15;
+    } else if(celsius !=''){
+        fahrenheit = (parseFloat(celsius) * 1.8) + 32;
+        kelvin=(parseFloat(celsius))+273.15;
+    }
+    else{
+        celsius=(parseFloat(kelvin))-273.15;
+        fahrenheit=(parseFloat(kelvin)-273.15)*(9/5)+32;
+    }
+  
+
+
+    document.getElementById('fahrenheit').value = parseFloat(fahrenheit).toFixed(1);
+    document.getElementById('celsius').value = parseFloat(celsius).toFixed(1);
+    document.getElementById('kelvin').value = parseFloat(kelvin).toFixed(1);
+}
+
+
+function clearForm() {
+    document.getElementById('fahrenheit').value = '';
+    document.getElementById('celsius').value = '';
+    document.getElementById('kelvin').value = '';
+} 
